f59818c64ebcdbe5e7581fa5965889ea4f8cd6cb
hedekar <afterthebeep@gmail.com>
2015-11-21 07:56:44 +0000

    Friday's work - reworking functions

--- Colour.cpp
+++ Colour.cpp
@@ -1,4 +1,5 @@
-#include <math.h>	// needed for log2, ceil
+#include <math.h>	// needed for log2, ceil, floor
+#include <algorithm.h> // needed for min function
 // to determine how many bins for the histogram we need to know pixel size of our frames
 
 const int WIDTH = 32;
@@ -19,6 +20,17 @@ float gConvert(rgb[3]){
 	return rgb[1]/(rgb[0]+rgb[1]+rgb[2]);
 }
 
+// void chromConvert(videofile){
+// 	VideoCapture original();
+// 	Mat frame;
+// 	if(!original.open(videofile)){
+// 		cerr << "Video file not opened." << endl;
+// 	}else{
+// 		original.grab();
+// 		original.retrieve(frame, 0);
+// 	}
+// }
+
 
 ////code for histogram
 
@@ -27,26 +39,44 @@ int binNum(int w=WIDTH){
 	return ceil(1+log2(w));
 }
 
+// This function returns the I value from any two histograms
+float getI(float histT[HEIGHT][HEIGHT], float histTT[HEIGHT][HEIGHT]){
+	float sum = 0;
+	for(int i=0;i<HEIGHT;i++){
+		for(int j=0;j<HEIGHT;j++){
+			sum = sum+min( histT[i][j], histTT[i][j] );
+		}
+	}
+}
+
+
 	// function to divide 0-1 into binNum bins goes here (we'll use upper bounds)
 int main(){ // maybe command-line filename input?
 	int wBins = binNum(WIDTH);
 	int hBins = binNum(HEIGHT);
-	float wHistBins[wBins]={};
+	/*float wHistBins[wBins]={};
 	float hHistBins[hBins]={};
 	for(int i=0; i<wBins; i++){//actually, is this needed in the code?
 		histBins[i]=i*(1/wBins);		// Upper bounds for each bin.
 	}
 	for(int i=0; i<hBins; i++){
 		histBins[i]=i*(1/hBins);		// w and h are likely =, but we create one for each in case image ratio isn't square
-	}
+	}*/
 
-    //// This bit is coded for columns.  We'll need to do the same for rows.
-	// create 2-d array for current histogram
-	int frameHist[hBins][hBins] = {};
-	// for each pixel in this col/row turn rgb to rg, see what bin it fits in, increment count of hist bin
-	for(int i=1; i<HEIGHT; i++){
-		int rgb[3]={}//get pixel rgb here. (we'll need to wrap this for loop in a couple for loops to cycle through cols & frames)
-		int rg[2]={rConvert(rgb),gConvert(rgb)};
-		framHist[ floor(rg[0]*hBins) ][ floor(rg[1]*hbins) ]++; //increment the hist bin
+	// for (int j=1; j<FRAMENUM; j++){
+		// for (int k=0; k<FColNum; k++){
+			// create 2-d array for current histogram
+			float fColHist[hBins][hBins] = {};
+			// for each pixel in this col/row turn rgb to rg, see what bin it fits in, increment count of hist bin
+
+			for(int i=1; i<HEIGHT; i++){
+				int rgb[3]={  }//get pixel rgb here. (we'll need to wrap this for loop in a couple for loops to cycle through cols & frames)
+				int rg[2]={rConvert(rgb),gConvert(rgb)};
+
+				int rBin = floor(rg[0]*hBins);
+				int gBin = floor(rg[1]*hBins);
+				fColHist[rBin][gBin] = fColHist[rBin][gBin] + (1/HEIGHT); //increment the hist bin
+			}
+		}
 	}
 }
